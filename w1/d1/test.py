6ee45fa28d5007516b24b7bd51a4ecb241bb0d05
spedi123 <peteran8604@gmail.com>
2022-07-05 13:09:29 -0700

    for loop basic1  assignment

--- w1/d1/test.py
+++ w1/d1/test.py
@@ -2,9 +2,41 @@
 # print(type(3.9))
 # print(type(3j))
 
-first_name = "peter"
-x = "2"
+# first_name = 0
+# x = "2"
 
-print(int(x) + 2)
+# print(int(x) + 2)
 
-xxx
+# print(True)
+# print(False)
+
+# if first_name:
+#     print("yes")
+# else:
+#     print("no")
+
+person = {
+    'first_name': 'peter',
+    'last_name': 'an',
+    'age': 36
+}
+
+brothers = [
+    {
+        'first_name': 'peter',
+        'last_name': 'an',
+        'age': 36
+    },
+    {
+        'first_name': 'tim',
+        'last_name': 'song',
+        'age': 33
+    },
+    {
+        'first_name': 'steve',
+        'last_name': 'lee',
+        'age': 32
+    }
+]
+
+print(brothers[0]['first_name'])
