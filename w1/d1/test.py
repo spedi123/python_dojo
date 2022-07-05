26ec64315c6eff54a5c14d20f18855b00d8ce96d
spedi123 <peteran8604@gmail.com>
2022-07-05 16:43:39 -0700

    complete practice assingment(functions basic i)

--- w1/d1/test.py
+++ w1/d1/test.py
@@ -1,42 +1,9 @@
-# print(type(24))
-# print(type(3.9))
-# print(type(3j))
+def multiply(num_list, num):
+    for x in range(len(num_list)):
+        num_list[x] *= num
+    return num_list
 
-# first_name = 0
-# x = "2"
 
-# print(int(x) + 2)
-
-# print(True)
-# print(False)
-
-# if first_name:
-#     print("yes")
-# else:
-#     print("no")
-
-person = {
-    'first_name': 'peter',
-    'last_name': 'an',
-    'age': 36
-}
-
-brothers = [
-    {
-        'first_name': 'peter',
-        'last_name': 'an',
-        'age': 36
-    },
-    {
-        'first_name': 'tim',
-        'last_name': 'song',
-        'age': 33
-    },
-    {
-        'first_name': 'steve',
-        'last_name': 'lee',
-        'age': 32
-    }
-]
-
-print(brothers[0]['first_name'])
+a = [2, 4, 10, 16]
+b = multiply(a, 5)
+print(b)
