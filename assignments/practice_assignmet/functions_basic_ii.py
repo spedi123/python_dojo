5b1c0cd970d7888f2273309665d643c884e55fdb
spedi123 <peteran8604@gmail.com>
2022-07-05 18:06:00 -0700

    complete practice assignment function basic ii

---
+++ assignments/practice_assignmet/functions_basic_ii.py
@@ -0,0 +1,59 @@
+# # 1. Countdown 5-0
+# def count_down(num):
+#     countDown = []
+#     for i in range(num, -1, -1):
+#         countDown.append(i)
+#     return countDown
+
+
+# print(count_down(5))
+
+# # 2. Print and Return
+# pr = [1, 2]
+
+
+# def test_pr():
+#     print(pr[0])
+#     return pr[1]
+
+
+# print(test_pr())
+
+# # 3. First Plus Length
+
+# def plus(arr):
+#     x = arr[0] + len(arr)
+#     return x
+
+
+# print(plus([1, 2, 3, 4, 5, 6]))
+
+
+# # 4. Values Greater than Second
+# value1 = [1, 2, 3, 4, 5]
+# value2 = [1, 2, 3]
+# value3 = []
+
+
+# def comparing(arr):
+#     if len(arr) < 2:
+#         return False
+#     new_list = []
+#     for i in range(0, len(arr)):
+#         if arr[i] > arr[1]:
+#             new_list.append(arr[i])
+#     return new_list
+
+
+# print(comparing(value3))
+
+# # 5. This Length, That Value
+
+# def length_value(length, value):
+#     new_list = []
+#     for i in range(length):
+#         new_list.append(value)
+#     return new_list
+
+
+# print(length_value(5, 10))
