3258a04a12964a6a401d59f145f6998715d5ae59
spedi123 <peteran8604@gmail.com>
2022-07-06 15:09:39 -0700

    complete bank account assignment

--- assignments/core_assignment/bankaccount.py
+++ assignments/core_assignment/bankaccount.py
@@ -1,30 +1,51 @@
 class BankAccount:
-    # don't forget to add some default values for these parameters!
-    def __init__(self, int_rate, balance):
+    bank = 'bank of dojo'
+
+    def __init__(self, balance=0, int_rate=0.01,):
+        self.balance = balance
         self.int_rate = int_rate
-        self.balace = balance
         # your code here! (remember, instance attributes go here)
         # don't worry about user info here; we'll involve the User class soon
 
     def deposit(self, amount):
-        self.deposit += amount
+        self.balance += amount
+        return self
         # your code here
 
     def withdraw(self, amount):
-        self.withdraw -= amount
+        if amount > self.balance:
+            print('Insufficient funds: Charging a $5 fee')
+            self.balance -= 5
+            self.balance -= amount
+        else:
+            self.balance -= amount
+        return self
+
         # your code here
 
     def display_account_info(self):
+        print(f'bank: {self.bank}')
         print(f'balance: {self.balance}')
+        return self
 
     def yield_interest(self):
-        if self.balace > 0:
+        if self.balance > 0:
             self.balance = (self.balance * self.int_rate) + self.balance
-            return self
+        else:
+            self.balance
+        return self
+
+    @classmethod
+    def bank_account_info(cls, bank_name):
+        cls.bank = bank_name
 
 
-balance_a = BankAccount(0.01, 1000)
-balance_b = BankAccount(0.01, 2000)
-balance_c = BankAccount(0.01, 3000)
+user_a = BankAccount(1000)
+user_b = BankAccount(2000)
+user_c = BankAccount(3000)
 
-balance_a.disply_account_info()
+user_a.deposit(1000).deposit(3000).deposit(2000).withdraw(
+    500).yield_interest().display_account_info()
+user_b.deposit(500).deposit(2000).withdraw(2200).withdraw(
+    520).withdraw(1500).yield_interest().display_account_info()
+user_c.deposit(100).withdraw(3500).yield_interest().display_account_info()
