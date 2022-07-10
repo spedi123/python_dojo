3c8e93c1b78ae0fb383c51a5263409150038fe6f
spedi123 <peteran8604@gmail.com>
2022-07-09 19:03:56 -0700

    complete html table assigment

---
+++ assignments/core_assignment/html_table/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+# this is going to move in the future
+
+
+@app.route('/')
+def users():
+    users = [
+        {'first_name': 'Michael', 'last_name': 'Choi'},
+        {'first_name': 'John', 'last_name': 'Supsupin'},
+        {'first_name': 'Mark', 'last_name': 'Guillen'},
+        {'first_name': 'KB', 'last_name': 'Tonel'}
+    ]
+    return render_template('index.html', users=users)
+# end of moving area
+
+
+# this must be on the bottom of this file
+if __name__ == "__main__":
+    app.run(debug=True)
