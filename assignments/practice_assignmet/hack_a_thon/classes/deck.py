298b451964fbea9b3066131e39f613891830800d
spedi123 <peteran8604@gmail.com>
2022-07-10 22:04:04 -0700

    practice hack a thone.

--- assignments/practice_assignmet/hack_a_thon/classes/deck.py
+++ assignments/practice_assignmet/hack_a_thon/classes/deck.py
@@ -1,28 +1,38 @@
 from . import card
+import random
+
 
 class Deck:
 
-
-    def __init__( self ):
-        suits = [ "spades" , "hearts" , "clubs" , "diamonds" ]
+    def __init__(self):
+        suits = ["spades", "hearts", "clubs", "diamonds"]
         self.cards = []
 
         for s in suits:
-            for i in range(1,14):
+            for i in range(1, 14):
                 str_val = ""
                 if i == 1:
                     str_val = "Ace"
                 elif i == 11:
                     str_val = "Jack"
+                    point_val = 10
                 elif i == 12:
                     str_val = "Queen"
+                    point_val = 10
                 elif i == 13:
                     str_val = "King"
+                    point_val = 10
                 else:
                     str_val = str(i)
-                self.cards.append( card.Card( s , i , str_val ) )
+                self.cards.append(card.Card(s, point_val, str_val))
 
     def show_cards(self):
         for card in self.cards:
             card.card_info()
 
+    def shuffle_cards(self):
+        random.shuffle(self.cards)
+
+    def draw(self):
+        draw_game = self.cards.pop(0)
+        return draw_game
