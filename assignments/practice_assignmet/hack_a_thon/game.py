54bd5f873e756bf72ca9354a3935833afba3ecc9
spedi123 <peteran8604@gmail.com>
2022-07-10 22:03:43 -0700

    practice hack a thone

--- assignments/practice_assignmet/hack_a_thon/game.py
+++ assignments/practice_assignmet/hack_a_thon/game.py
@@ -1,5 +1,14 @@
 from classes.deck import Deck
 
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.is_bursted = False
+        self.sum = 0
+        self.hands = []
+
+
 bicycle = Deck()
 
 bicycle.show_cards()
