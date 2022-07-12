4102c24c678096e0782e0b6342e8ce39e0bab6ef
spedi123 <peteran8604@gmail.com>
2022-07-12 13:33:40 -0700

    server, index.html, result.html finished.

---
+++ assignments/core_assignment/dojo_survey/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "peteranishere"
+# this is going to move in the future
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/process', methods=['POST'])
+def survey_info():
+    print(request.form)
+    for key in request.form:
+        session[key] = request.form[key]
+    return redirect('/result')
+
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+# end of moving area
+
+
+# this must be on the bottom of this file
+if __name__ == "__main__":
+    app.run(debug=True)
