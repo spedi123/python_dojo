325f2ba00b1cfe478f98764508e68314ef5d5554
spedi123 <peteran8604@gmail.com>
2022-07-16 13:20:15 -0700

    complete users crud assignment

--- assignments/practice_assignmet/user_cr/model/model_user.py
+++ assignments/practice_assignmet/user_cr/model/model_user.py
@@ -13,6 +13,9 @@ class Users:
         self.updated_at = data['updated_at']
     # Now we use class methods to query our database
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
@@ -27,6 +30,22 @@ class Users:
 
     @classmethod
     def save(cls, data):
-        query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(first_name)s , %(last_name)s , %(email)s , NOW() , NOW() );"
+        query = "INSERT INTO users ( first_name , last_name , email) VALUES ( %(first_name)s , %(last_name)s , %(email)s);"
         # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('users_schema').query_db(query, data)
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s WHERE id=%(id)s;"
+        return connectToMySQL('users_schema').query_db(query, data)
+
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s"
+        result = connectToMySQL('users_schema').query_db(query, data)
+        return cls(result[0])
+
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s"
         return connectToMySQL('users_schema').query_db(query, data)
--- assignments/practice_assignmet/user_cr/server.py
+++ assignments/practice_assignmet/user_cr/server.py
@@ -1,8 +1,7 @@
-from operator import methodcaller
 from flask import Flask, render_template, redirect, request, session
 from model.model_user import Users
 app = Flask(__name__)
-app.secret_key = "peteran is here"
+app.secret_key = "peteranishere"
 
 
 @app.route("/users")
@@ -24,6 +23,32 @@ def add_user():
         "email": request.form["email"]
     }
     Users.save(data)
+    return redirect('/users')
+
+
+@app.route("/users/detail/<int:id>")
+def user_detail(id):
+    data = {"id": id}
+    return render_template("user_detail.html", user=Users.get_one(data))
+
+
+@app.route("/users/edit/<int:id>")
+def user_edit(id):
+    data = {"id": id}
+    return render_template("edit_user.html", user=Users.get_one(data))
+
+
+@app.route("/users/update", methods=['POST'])
+def user_update():
+    print(request.form)
+    Users.update(request.form)
+    return redirect('/users')
+
+
+@app.route("/users/delete/<int:id>")
+def user_delete(id):
+    data = {"id": id}
+    Users.delete(data)
     return redirect('/users')
 
 
