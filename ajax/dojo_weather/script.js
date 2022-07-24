4964fef299bbaba22aa82690de8429ab40b5822e
spedi123 <peteran8604@gmail.com>
2022-07-23 22:12:15 -0700

    pratice for api

--- ajax/dojo_weather/script.js
+++ ajax/dojo_weather/script.js
@@ -26,4 +26,37 @@ function change(e) {
   }
 }
 
-// api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}
+function k_c(e) {
+  return Math.floor(e - 273);
+}
+
+function get_city_weather(event, city) {
+  event.preventDefault();
+  const locations = {
+    burbank: { lat: "34.18", lon: "-118.30" },
+    chicago: { lat: "41.87", lon: "-87.62" },
+    dallas: { lat: "32.77", lon: "-96.79" },
+  };
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${locations[city].lat}&lon=${locations[city].lon}&appid=d36ac81c4e2b436c8b85b85016cfcdb6`;
+  console.log(city, URL);
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      var forecast_today = document.querySelector(".forcast_today");
+      var temp_max = `${data.main.temp_max}`;
+      var temp_min = `${data.main.temp_min}`;
+      forecast_today.innerHTML = `
+      <div class="forecast-detail-today">
+        <p>Today</p>
+        <img src="./assets/some_rain.png" alt="some_rain" />
+        <p>some rain</p>
+        <p class="numberOfDegree">
+          <span class="high" id="temp1">${k_c(temp_max)}°</span>
+          <span class="low" id="temp2">${k_c(temp_min)}°</span>
+        </p>
+      </div>
+      `;
+    })
+    .catch((err) => console.log(err));
+}
