f9f32a4e30336aca1904f968f55ac64482f6978e
spedi123 <peteran8604@gmail.com>
2022-07-29 08:03:38 -0700

    almost done project

--- projects/flask_app/static/js/list.js
+++ projects/flask_app/static/js/list.js
@@ -19,7 +19,6 @@ function get_cocktail_list(value) {
                             ingredients.push(drink[key2])
                         }
                     }
-                console.log(img_url)
                 card_list.innerHTML += 
                 `
                 <div class="card" style="width: 18rem;">
@@ -43,3 +42,49 @@ function get_cocktail_list(value) {
         })
         .catch(err => console.log(err) )   
 }
+
+var search_form = document.querySelector('.search_form')
+
+search_form.addEventListener('submit', function(e){
+    e.preventDefault()
+    let search_bar = document.querySelector('#search_bar').value
+    
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search_bar}`)
+    .then(response => response.json() )
+    .then((data) =>{
+        console.log(data)
+        var card_show = document.querySelector('.card_show')
+        for (const key in data.drinks) {
+            let drink = data.drinks[key]
+            let img_url = drink.strDrinkThumb
+            let name = drink.strDrink
+            let instruction = drink.strInstructions
+            let ingredients = []
+                for (const key2 in drink) {
+                    if (key2.includes('strIngredient')) {
+                        ingredients.push(drink[key2])
+                    }
+                }
+        card_show.innerHTML += 
+        `
+            <div class="card1" style="width: 18rem;">
+                <img src="${img_url}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-instruction">${instruction}</p>
+                <p class="card_ingredient">${ingredients} </p>
+                <div id="view_button">
+                <form action="/api/cocktail/create" method="post" id="cocktail_form">          
+                    <input type="hidden" name="name" value=${name} />
+                    <input type="hidden" name="instruction" value=${instruction}/>
+                    <input type="hidden" name="ingredient" value=${ingredients}/>
+                    <input type="hidden" name="img_url" value=${img_url}/>
+                    <button class="btn btn-success">Save My list</button>
+                </form>
+                </div>
+            </div>`
+        }
+        })
+    .catch(err => console.log(err) )   
+
+})
